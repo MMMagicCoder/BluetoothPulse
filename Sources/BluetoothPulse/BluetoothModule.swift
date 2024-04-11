16d21594f739dad64f29aa4b089588baebaf49fd
Mohammad Mahdi Moayeri <mmahdimoayeri@gmail.com>
2024-04-11 18:12:15 +0330

    add CoreBluetoothModule class

---
+++ Sources/BluetoothPulse/BluetoothModule.swift
@@ -0,0 +1,176 @@
+import SwiftUI
+import CoreBluetooth
+
+class CoreBluetoothModule: NSObject, ObservableObject, CBPeripheralDelegate, CBCentralManagerDelegate {
+    @Published var isBleOn: Bool = false
+    @Published var isSearching: Bool = false
+    @Published var isConnected: Bool = false
+    
+    @Published var discoverPeripherals: [Peripheral] = []
+    @Published var discoverCharacteristics: [Characteristic] = []
+    @Published var discoverServices: [Service] = []
+    @Published var connectedPeripheral: Peripheral!
+    
+    private var centralManager: CBCentralManager!
+    
+    private let serviceUUID: CBUUID = CBUUID()
+    
+    override init() {
+        super.init()
+        
+        //        `CBCentralManagerOptionShowPowerAlertKey: true` used to show user turn on bluetooth if it is off.
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
+    }
+    
+    private func resetConfiguration() {
+        withAnimation {
+            isSearching = false
+            isConnected = false
+            
+            discoverPeripherals = []
+            discoverCharacteristics = []
+            discoverServices = []
+        }
+    }
+    
+    //    MARK: Controling Functions
+    func startScan() {
+        // 'CBCentralManagerScanOptionAllowDuplicatesKey: false' used for not showing duplicate Peripherals.
+        let option = [CBCentralManagerScanOptionAllowDuplicatesKey: false]
+        centralManager?.scanForPeripherals(withServices: nil, options: option)
+        print("Scan Started...")
+        isSearching = true
+    }
+    
+    func stopScan() {
+        disconnectPeripheral()
+        centralManager?.stopScan()
+        print("Scan Stoped!")
+        isSearching = false
+    }
+    
+    func connectPeripheral(_ selectedPeripheral: Peripheral?) {
+        guard let connectedPeripheral = selectedPeripheral else { return }
+        self.connectedPeripheral = selectedPeripheral
+        centralManager.connect(connectedPeripheral.peripheral, options: nil)
+    }
+    
+    func disconnectPeripheral() {
+        guard let connectedPeripheral = connectedPeripheral else { return }
+        centralManager.cancelPeripheralConnection(connectedPeripheral.peripheral)
+    }
+    
+    //MARK: CoreBluetooth CentralManager Delegete Functions
+    func didUpdateState(_ central: CBCentralManager) {
+        guard central.state == .poweredOn else { return isBleOn = false}
+        isBleOn = true
+        startScan()
+    }
+    
+    func didDiscover(_ central: CBCentralManager, peripheral: CBPeripheral, advertisementData: [String : Any], rssi: NSNumber) {
+        guard rssi.intValue < 0 else { return }
+        
+        let peripheralName = advertisementData[CBAdvertisementDataLocalNameKey] as? String ?? nil
+        var _name = "NoName"
+        
+        if peripheralName != nil {
+            _name = String(peripheralName!)
+        } else if peripheral.name != nil {
+            _name = String(peripheral.name!)
+        }
+        
+        let discoveredPeripheral: Peripheral = Peripheral(
+            _peripheral: peripheral,
+            _name: _name,
+            _advertisementData: advertisementData,
+            _rssi: rssi,
+            _discoverNumber: 0)
+        
+        if let index = discoverPeripherals.firstIndex(where: { $0.peripheral.identifier.uuidString == peripheral.identifier.uuidString }) {
+            if discoverPeripherals[index].discoverNumber % 50 == 0 {
+                discoverPeripherals[index].name = _name
+                discoverPeripherals[index].rssi = rssi.intValue
+                discoverPeripherals[index].discoverNumber += 1
+            } else {
+                discoverPeripherals[index].discoverNumber += 1
+            }
+        } else {
+            discoverPeripherals.append(discoveredPeripheral)
+            self.isSearching = false
+        }
+    }
+    
+    func didConnect(_ center: CBCentralManager, peripheral: CBPeripheral) {
+        guard let connectedPeripheral = connectedPeripheral else { return }
+        self.isConnected = true
+        connectedPeripheral.peripheral.delegate = self
+        connectedPeripheral.peripheral.discoverServices(nil)
+    }
+    
+    func didFailToConnect(_ central: CBCentralManager, peripheral: CBPeripheral, error: Error?) {
+        if let error = error {
+            print("Failed to connect to peripheral: \(error.localizedDescription)")
+        } else {
+            print("Failed to connect to peripheral with unknown error.")
+        }
+        disconnectPeripheral()
+    }
+    
+    func didDisconnect(_ central: CBCentralManager, peripheral: CBPeripheral, error: Error?) {
+        if let error = error {
+            print("Disconnected from peripheral '\(peripheral.identifier.uuidString)' with error: \(error.localizedDescription)")
+        } else {
+            print("Disconnected from peripheral '\(peripheral.identifier.uuidString)'")
+        }
+        resetConfiguration()
+    }
+    
+    func connectionEventDidOccur(_ central: CBCentralManager, event: CBConnectionEvent, peripheral: CBPeripheral) {
+        
+    }
+    
+    func willRestoreState(_ central: CBCentralManager, dict: [String : Any]) {
+        
+    }
+    
+    func didUpdateANCSAuthorization(_ central: CBCentralManager, peripheral: CBPeripheral) {
+        
+    }
+    
+    //MARK: CoreBluetooth Peripheral Delegate Functions
+    func didDiscoverServices(_ peripheral: CBPeripheral, error: Error?) {
+        peripheral.services?.forEach { service in
+            let foundService = Service(_uuid: service.uuid, _service: service)
+            
+            discoverServices.append(foundService)
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func didDiscoverCharacteristics(_ peripheral: CBPeripheral, service: CBService, error: Error?) {
+        service.characteristics?.forEach { characteristic in
+            let foundCharacteristic = Characteristic(
+                _characteristic: characteristic,
+                _description: "",
+                _uuid: characteristic.uuid,
+                _readValue: "",
+                _service: characteristic.service!)
+            
+            discoverCharacteristics.append(foundCharacteristic)
+            peripheral.readValue(for: characteristic)
+        }
+    }
+    
+    func didUpdateValue(_ peripheral: CBPeripheral, characteristic: CBCharacteristic, error: Error?) {
+        guard let characteristicValue = characteristic.value else { return }
+        
+        if let index = discoverCharacteristics.firstIndex(where: { $0.uuid.uuidString == characteristic.uuid.uuidString }) {
+            
+            discoverCharacteristics[index].readValue = characteristicValue.map({ String(format:"%02x", $0) }).joined()
+        }
+    }
+    
+    func didWriteValue(_ peripheral: CBPeripheral, descriptor: CBDescriptor, error: Error?) {
+        
+    }
+}
